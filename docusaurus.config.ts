50746523bfba07e9c8b1efe9bf460fd2f0de1b41
hieunv <hieunv.dev@gmail.com>
2025-09-29 23:48:09 +0700

    config: change onBrokenLinks to warn instead of throw
    - Allow build to continue with warnings instead of failing on broken links
    - Helpful during development when some links may be temporarily broken
    - Maintains link checking while preventing build failures

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,7 +25,7 @@ const config: Config = {
   organizationName: 'behitek', // Usually your GitHub org/user name.
   projectName: 'beli5', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set

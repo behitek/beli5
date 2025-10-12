72b1c8ea7086ef93875ed8b109d66c179fd5beab
hieunv <hieunv.dev@gmail.com>
2025-10-12 16:42:20 +0700

    fix: change onBrokenLinks behavior from 'throw' to 'warn' in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
   organizationName: 'behitek', // Usually your GitHub org/user name.
   projectName: 'beli5', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
